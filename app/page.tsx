902c6e033c730655f0345afad83547e132b91cf8
brianmichaelu <brianmichaelu@gmail.com>
2026-05-19 19:53:45 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -108,7 +108,7 @@ export default function Home() {
 
     <div className="overflow-hidden rounded-[2rem] bg-[#FAF7F0] shadow-2xl">
       <Image
-        src="/hotel-hero.jpg"
+        src="/exterior.jpg"
         alt="Tegeta Palm Hotel exterior"
         width={1200}
         height={800}
