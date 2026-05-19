b176bd8e7daaf67dd7a8663a7596f3d8a3c03451
brianmichaelu <brianmichaelu@gmail.com>
2026-05-20 00:21:54 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -318,15 +318,15 @@ export default function Home() {
             <div className="mt-6 h-72 rounded-3xl bg-white p-5 shadow-xl ring-1 ring-black/5">
               {/* Replace this box with Google Maps embed iframe */}
               <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d15851.723688936343!2d39.1855733!3d-6.65548415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2stz!4v1779224426122!5m2!1sen!2stz" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>"
-                  width="100%"
-                  height="100%"
+                  src="<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d15851.723688936343!2d39.1855733!3d-6.65548415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2stz!4v1779224426122!5m2!1sen!2stz" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">"
+                   width="100%"
+                   height="100%"
                   style={{ border: 0 }}
                   allowFullScreen
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
                   className="rounded-2xl"
-          
+              />
               
             </div>
           </div>
